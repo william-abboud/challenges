08a33106b42a9390b166b55743637642aedac1ec
William <william_y_abboud@abv.bg>
2022-03-13 21:08:44 +0200

    Refactored errorMiddleware to handle HTTPErrors and any Errors

--- server/src/middlwares/errorMiddleware.ts
+++ server/src/middlwares/errorMiddleware.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import HttpError from "../exceptions/HttpError";
 
-const errorMiddleware = (error: HttpError, req: Request, res: Response, next: NextFunction) => {
+const errorMiddleware = (
+  error: HttpError | Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   if (error instanceof HttpError) {
-    res.status(error.status).send({ error: error });
+    res.status(error.status).send({ message: error.message, type: error.type });
+  } else if (error instanceof Error) {
+    res.status(500).send({ message: "Something went wrong!", type: "UnknownError" });
   } else {
     next();
   }
