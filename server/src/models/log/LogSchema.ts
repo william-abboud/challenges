b8099a0c962069f973ab790a4ae91f2f2aeef8c0
william-abboud <william_y_abboud@abv.bg>
2022-03-19 22:30:52 +0200

    Removed need for id

--- server/src/models/log/LogSchema.ts
+++ server/src/models/log/LogSchema.ts
@@ -2,12 +2,6 @@ import { Schema } from "mongoose";
 import ILogDocument from "./ILogDocument";
 
 const LogSchema = new Schema<ILogDocument>({
-    id: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        unique: true,
-        auto: true,
-    },
     content: {
         type: String,
         required: true,
