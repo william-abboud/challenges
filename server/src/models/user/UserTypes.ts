f48599ef72ccbc9e3b05949c3325cad3ae6addc9
william-abboud <william_y_abboud@abv.bg>
2022-03-19 21:58:40 +0200

    Added UserSafeDetails type

--- server/src/models/user/UserTypes.ts
+++ server/src/models/user/UserTypes.ts
@@ -3,7 +3,8 @@ import IUser from "./IUser";
 type NewUserDetails = Omit<IUser, "id" | "passwordHash"> & { password: string };
 type NewUserDetailsDBReady = Omit<IUser, "id">;
 type UserDetails = Omit<IUser, "passwordHash">;
+type UserSafeDetails = Omit<UserDetails, "email">;
 type UserLoginDetails = Pick<IUser, "email"> & { password: string };
 type UserIdentity = Pick<IUser, "id">;
 
-export { NewUserDetails, NewUserDetailsDBReady, UserDetails, UserLoginDetails, UserIdentity };
+export { NewUserDetails, NewUserDetailsDBReady, UserDetails, UserLoginDetails, UserIdentity, UserSafeDetails };
