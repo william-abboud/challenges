58953ebe18fa7b4e4d9ca1ebb6064b5e92293c1b
william-abboud <william_y_abboud@abv.bg>
2022-03-20 23:05:12 +0200

    Added GetAll and Start endpoints

--- server/src/controllers/participants/IParticipantsController.ts
+++ server/src/controllers/participants/IParticipantsController.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import IPaginationOptions from "../../interfaces/pagination/IPaginationOptions";
 
 interface IParticipantsController {
   participate(req: Request, res: Response, next: NextFunction): void;
+  start(req: Request, res: Response, next: NextFunction): void;
+  getAll(req: Request<unknown, unknown, unknown, IPaginationOptions>, res: Response, next: NextFunction): void;
 }
 
 export default IParticipantsController;
--- server/src/controllers/participants/ParticipantsController.ts
+++ server/src/controllers/participants/ParticipantsController.ts
@@ -1,15 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import { param } from "express-validator";
+import { param, query } from "express-validator";
 import { inject } from "inversify";
-import { controller, httpPut } from "inversify-express-utils";
+import { controller, httpPut, httpGet } from "inversify-express-utils";
 
 import ChallengeNotFoundError from "../../exceptions/ChallengeNotFoundError";
 import HttpError from "../../exceptions/HttpError";
 import UserAlreadyParticipantInChallengeError from "../../exceptions/UserAlreadyParticipantInChallengeError";
 import UserDoesNotExistError from "../../exceptions/UserDoesNotExistError";
+import IPaginationOptions from "../../interfaces/pagination/IPaginationOptions";
 import Locator from "../../locator";
 import validateApiArgs from "../../middlwares/apiValidationMiddleware";
 import authMiddleware from "../../middlwares/authMiddleware";
+import { ChallengeIdentity } from "../../models/challenge/ChallengeTypes";
+import { ParticipantIdentity } from "../../models/participant/ParticipantTypes";
 import { UserDetails } from "../../models/user/UserTypes";
 import IParticipantService from "../../services/participant/IParticipantService";
 import IParticipantsController from "./IParticipantsController";
@@ -20,6 +23,59 @@ class ParticipantsController implements IParticipantsController {
 
   constructor(@inject(Locator.ParticipantService) service: IParticipantService) {
     this.service = service;
+  }
+
+  @httpGet(
+    "/getAll",
+    query("page")
+      .customSanitizer((v) => v || 0)
+      .isNumeric()
+      .toInt(10),
+    query("size")
+      .customSanitizer((v) => v || 25)
+      .isNumeric()
+      .toInt(10)
+      .default(25),
+    validateApiArgs,
+  )
+  async getAll(
+    req: Request<unknown, unknown, unknown, IPaginationOptions>,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const { page, size } = req.query;
+
+    const paginationOptions = {
+      page,
+      size,
+    };
+
+    try {
+      const participants = await this.service.getPaginatedParticipants(paginationOptions);
+
+      res.send(participants);
+    } catch (err: unknown) {
+      next(err);
+    }
+  }
+
+  @httpPut(
+    "/start/:participantId/challenge/:challengeId",
+    authMiddleware,
+    param("participantId").isMongoId(),
+    param("challengeId").isMongoId(),
+    validateApiArgs
+  )
+  async start(req: Request<{ challengeId: ChallengeIdentity["id"], participantId: ParticipantIdentity["id"] }>, res: Response, next: NextFunction) {
+    const { challengeId, participantId } = req.params;
+
+    try {
+      const challenge = await this.service.startProgress(challengeId, participantId);
+
+      res.send(challenge);
+    } catch (error) {
+      next(error);
+    }
   }
 
   @httpPut(
