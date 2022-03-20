da2238c2a5859e2535de9277675b57446a51ee43
william-abboud <william_y_abboud@abv.bg>
2022-03-20 23:07:11 +0200

    Added ParticipantIdentity Type

--- server/src/models/participant/ParticipantTypes.ts
+++ server/src/models/participant/ParticipantTypes.ts
@@ -1,5 +1,6 @@
 import IParticipant from "./IParticipant";
 
 type ParticipantDetails = Omit<IParticipant, "id">;
+type ParticipantIdentity = Pick<IParticipant, "id">;
 
-export { ParticipantDetails };
+export { ParticipantDetails, ParticipantIdentity };
