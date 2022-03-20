cabb049e1b00fdda39e5ab3af693ef7b01b1f6bb
william-abboud <william_y_abboud@abv.bg>
2022-03-20 23:13:19 +0200

    Added getParticipantInChallenge, updateProgressStatus, аddProgress, getPaginatedParticipants methods,

--- server/src/repositories/participant/IParticipantRepository.ts
+++ server/src/repositories/participant/IParticipantRepository.ts
@@ -1,10 +1,20 @@
+import ProgressStatus from "../../enums/ProgressStatus";
+import IPaginationOptions from "../../interfaces/pagination/IPaginationOptions";
 import IParticipant from "../../models/participant/IParticipant";
 import { ParticipantDetails } from "../../models/participant/ParticipantTypes";
+import IProgress from "../../models/progress/IProgress";
 
 interface IParticipantRepository {
   createParticipant(participant: ParticipantDetails): Promise<IParticipant>;
   getParticipant(userId: string): Promise<IParticipant | null>;
   getUserParticipantInChallenge(challengeId: string, userId: string): Promise<IParticipant | null>;
+  getParticipantInChallenge(
+    challengeId: string,
+    userId: string,
+  ): Promise<IParticipant | null>
+  updateProgressStatus(participantId: string, challengeId: string, status: ProgressStatus): Promise<IParticipant>;
+  addProgress(participantId: string, progress: IProgress): Promise<IParticipant | null>;
+  getPaginatedParticipants(paginationOptions: IPaginationOptions): Promise<IParticipant[]>;
 }
 
 export default IParticipantRepository;
--- server/src/repositories/participant/ParticipantRepository.ts
+++ server/src/repositories/participant/ParticipantRepository.ts
@@ -6,6 +6,9 @@ import IParticipantDocument from "../../models/participant/IParticipantDocument"
 import { ParticipantDetails } from "../../models/participant/ParticipantTypes";
 import IParticipantRepository from "./IParticipantRepository";
 import Locator from "../../locator";
+import ProgressStatus from "../../enums/ProgressStatus";
+import IProgress from "../../models/progress/IProgress";
+import IPaginationOptions from "../../interfaces/pagination/IPaginationOptions";
 
 @injectable()
 class ParticipantRepository implements IParticipantRepository {
@@ -13,6 +16,12 @@ class ParticipantRepository implements IParticipantRepository {
 
   constructor(@inject(Locator.ParticipantModel) model: Model<IParticipantDocument>) {
     this.model = model;
+  }
+
+  async getPaginatedParticipants({ page, size }: IPaginationOptions): Promise<IParticipant[]> {
+    const participants = await this.model.find({}).limit(size).skip(size * page);
+
+    return participants.map((participant) => participant.toObject<IParticipant>());
   }
 
   async getParticipant(userId: string): Promise<IParticipant | null> {
@@ -35,6 +44,65 @@ class ParticipantRepository implements IParticipantRepository {
     });
 
     return participant ? participant.toObject<IParticipant>() : null;
+  }
+
+  async getParticipantInChallenge(
+    challengeId: string,
+    userId: string,
+  ): Promise<IParticipant | null> {
+    const participant = await this.model.findOne({
+      userId,
+      progresses: {
+        $elemMatch: {
+          challengeId,
+        },
+      }
+    });
+
+    return participant ? participant.toObject<IParticipant>() : null;
+  }
+
+  async addProgress(participantId: string, progress: IProgress): Promise<IParticipant | null> {
+    const participant = await this.model.findOneAndUpdate({
+      _id: participantId,
+    }, {
+      $push: {
+        progresses: progress,
+      }
+    }, {
+      new: true,
+    });
+
+    return participant ? participant.toObject<IParticipant>() : null;
+  }
+
+  async updateProgressStatus(participantId: string, challengeId: string, status: ProgressStatus): Promise<IParticipant> {
+    const participantDoc = await this.model.findOneAndUpdate({
+      _id: participantId,
+      progresses: {
+        $elemMatch: {
+          challengeId,
+        }
+      }
+    }, {
+      $set: {
+        "progresses.$.status": status,
+      }
+    }, {
+      projection: {
+        "progresses.$": 1,
+        "userId": 1,
+        "name": 1,
+      }
+    });
+    
+    if (!participantDoc) {
+      throw new Error("Participant not found");
+    }
+
+    const participant = participantDoc.toObject<IParticipant>();
+
+    return participant;
   }
 
   async createParticipant(participant: ParticipantDetails): Promise<IParticipant> {
